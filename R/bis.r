181f87c3ad3841ccac0ae9b4c1ef12bef244a0aa
somakd <somakd@gmail.com>
2019-12-18 08:53:53 -0600

    temp update

--- R/bis.r
+++ R/bis.r
@@ -24,38 +24,77 @@
 #'  This will be null if only "PP" stopping criterion was used.  Here the last included variable
 #'  is the last one included by either "PP" or "eBIC" if criteria="both" was selected}
 #' @export
-bis <- function(X,y,lam=nrow(X)/ncol(X)^2,w = sqrt(nrow(X))/ncol(X),criteria="PP")
+
+bis <- function(X,y,lam=nrow(X)/ncol(X)^2,criteria="n")
 {
   p = ncol(X)
   n = nrow(X)
   ys = scale(y)
-
+  
   xbar = colMeans(X)
-
+  
   stopifnot(class(X) %in% c("dgCMatrix","matrix"))
-
+  
   if(class(X) == "dgCMatrix") {
     D = 1/sqrt(colMSD_dgc(X,xbar))
   }  else   {
     D = apply(X,2,sd)
     D = 1/D
   }
-  Xty = D*as.numeric(crossprod(X,ys))
-
-
+  
+  xty = D*as.numeric(crossprod(X,ys))
+  
+  
+  yty <- n - 1
+  xtx <- n - 1
+  
+  
   max.var = n; # Intially allocate for maximum of n variables.
-
-  model = integer(0L)
+  
+  model = integer(n)
   postprob = numeric(max.var+1)
-  R0 = NULL
-  v0 = NULL
-
+  
+  
+  
+  chol.factor = matrix(NA,max.var,max.var)
+  v0 = numeric(max.var)
+  sts = numeric(p)
+  u   = numeric(p)
+  
+  
   postprob[1] = -0.5*(n-1)*log(n) # The posterior probability of the null model
   cat("\n Including: ")
-  for(ii in 1:n)
+  
+  
+  # First variable
+  chol.factor[1,1] = sqrt(xtx + lam)
+  logp <- 0.5*log(lam)-0.5*log(xtx+lam)-0.5*(n-1)*log(yty - (xty/chol.factor[1,1])^2)
+
+  j = which.max(logp)
+  model[1] = j;
+  postprob[2] = logp[j]
+  
+  for(ii in 2:max.var)
   {
-    this <- addvar(model = model,x = X, ys = ys, xty = Xty, lam = lam, w = w,
-                   R0 = R0, v0 = v0,D = D,xbar = xbar)
+    # this <- addvar(model = model,x = X, ys = ys, xty = Xty, lam = lam, w = w,
+    #                R0 = R0, v0 = v0,D = D,xbar = xbar)
+    
+    
+    # Compute X0 solve( X0'X0 + lam*I  , X0'xj)
+    model.cur = model[1:{ii-1}
+    X0 = X[,model.cur],drop=FALSE]
+    xjc = (X[,j] - xbar[j])*D[j]
+    X0txj = crossprod(X0 , xjc)
+    temp1 = backsolve(chol.factor,X0txj,transpose = TRUE,k = ii-1L)
+    temp2 = D[model.cur] * backsolve(chol.factor,temp1,k = ii-1L)
+    temp3 = X0 %*% temp2 # the following is not needed as we are centering after two lines -sum(xbar[model.cur] * temp2)
+    temp4 = xjc - temp3;
+    temp4 = temp4 - mean(temp4)
+    
+    Xtz = D*crossprod(X,temp4)
+    
+    sts = sts - 
+    
     j = this$which.max
     if(this$logp[j] < postprob[ii])      break;
     cat(j,", ",sep = "")
@@ -65,6 +104,84 @@ bis <- function(X,y,lam=nrow(X)/ncol(X)^2,w = sqrt(nrow(X))/ncol(X),criteria="PP
     v0 = this$v
   }
   cat(" Done.\n")
-
+  
   return(list(model.pp = model, postprobs=postprob[1:ii]))
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# bis <- function(X,y,lam=nrow(X)/ncol(X)^2,w = sqrt(nrow(X))/ncol(X),criteria="PP")
+# {
+#   p = ncol(X)
+#   n = nrow(X)
+#   ys = scale(y)
+#   
+#   xbar = colMeans(X)
+#   
+#   stopifnot(class(X) %in% c("dgCMatrix","matrix"))
+#   
+#   if(class(X) == "dgCMatrix") {
+#     D = 1/sqrt(colMSD_dgc(X,xbar))
+#   }  else   {
+#     D = apply(X,2,sd)
+#     D = 1/D
+#   }
+#   Xty = D*as.numeric(crossprod(X,ys))
+#   
+#   
+#   max.var = n; # Intially allocate for maximum of n variables.
+#   
+#   model = integer(0L)
+#   postprob = numeric(max.var+1)
+#   R0 = NULL
+#   v0 = NULL
+#   
+#   postprob[1] = -0.5*(n-1)*log(n) # The posterior probability of the null model
+#   cat("\n Including: ")
+#   for(ii in 1:n)
+#   {
+#     this <- addvar(model = model,x = X, ys = ys, xty = Xty, lam = lam, w = w,
+#                    R0 = R0, v0 = v0,D = D,xbar = xbar)
+#     j = this$which.max
+#     if(this$logp[j] < postprob[ii])      break;
+#     cat(j,", ",sep = "")
+#     postprob[ii+1] <- this$logp[j]
+#     model = c(model,j)
+#     R0 = this$R
+#     v0 = this$v
+#   }
+#   cat(" Done.\n")
+#   
+#   return(list(model.pp = model, postprobs=postprob[1:ii]))
+# }
