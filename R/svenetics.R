5a40cf6e9d7575fd16e626059a3264b8ccc80e05
Debarshi Chakraborty <80968315+debarshi-statsforever@users.noreply.github.com>
2026-05-24 23:56:11 -0500

    Release version 4.0.0 (correcting previous commit message)

--- R/svenetics.R
+++ R/svenetics.R
@@ -4,7 +4,7 @@
 svenetics <- function() {
   app_dir <- system.file("app", package = "bravo")
   if (app_dir == "") {
-    stop("Could not find the app directory. Try re-installing svenetics.")
+    stop("Could not find the app directory. Please try re-installing svenetics.")
   }
   runApp(app_dir, display.mode = "normal")
 }
